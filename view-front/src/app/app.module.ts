b437409ac6725052b7c08903c054ca5534d7faca
jaguaryan <jaguaryan@localhost.localdomain>
2018-10-24 21:23:40 -0300

    Ajustando Format dos campos: Data e Valor

--- view-front/src/app/app.module.ts
+++ view-front/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
 
 import { TableModule } from 'primeng/table';
 
@@ -9,6 +12,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -23,7 +28,9 @@ import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.compo
     HttpClientModule,
     TableModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
